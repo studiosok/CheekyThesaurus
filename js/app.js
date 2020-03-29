a9e2d574902c93cc421b16ac0dd714d702375611
Kristen Studioso <studiosok@gmail.com>
2020-03-29 13:29:07 -0400

    updated final project

--- js/app.js
+++ js/app.js
@@ -1,59 +1,68 @@
 $(() => {
 
+  $('.search').submit((e) => {
+    e.preventDefault()
 
+    const query = $('#query').val()
+    console.log(query)
 
-$('#search').submit((e) => {
-  e.preventDefault()
-  console.log("I've been clicked")
+    urbanCall(query)
+    afterClick()
+  })
 
-  const query = $('#query').val()
-  console.log(query)
-
-  urbanCall(query)
-  afterClick()
-})
-
-
-function afterClick () {
-  console.log("toggling div")
-  $('#beforeClick').toggle()
-  $('#afterClick').toggle()
-  $('#showBox').text(query)
-}
-
-function displayResults (outputs) {
-    console.log(outputs)
-    outputs.forEach((output) => {
-      $('#results-table tbody').append(
-        `<tr>
-          <td>${output.word}</td>
-          <td>${output.definition}</td>
-        </tr>`
-      )
-    })
+  function afterClick () {
+    $('#beforeClick').toggle()
+    $('#afterClick').toggle()
+    $('#afterClicks').toggle()
+    $('.onReset').toggle()
+    $('#showBox').text(query)
   }
 
-
-async function urbanCall(searchTerm) {
-
-const url = 'https://api.urbandictionary.com/v0/define'
-  try {
-  const response = await axios.get(url, {
-    params: {
-      term: searchTerm
+  function displayResults (outputs) {
+      console.log(outputs)
+      outputs.forEach((output) => {
+        $('#showDefinition').append(
+            `${output.definition}`
+          )
+      })
     }
+
+    function displaySearchedWord (output) {
+            $('#showBox').text(
+              `${output.word}`)
+      }
+
+  async function urbanCall(searchTerm) {
+
+  const url = 'https://api.urbandictionary.com/v0/define'
+    try {
+    const response = await axios.get(url, {
+      params: {
+        term: searchTerm
+      }
+    })
+    // console.log(response.data.list[0].definition)
+    displayResults(response.data.list)
+    displaySearchedWord(response.data.list[0])
+    // $('#testing').text()
+    // Need to make list[] index random
+  } catch(error) {
+    console.log(error)
+  }
+
+  }
+
+  $('.reset').click(() => {
+    reset()
   })
-  // console.log(response.data.list[0].definition)
-  displayResults(response.data.list)
-  // $('#testing').text()
-  // Need to make list[] index random
-} catch(error) {
-  console.log(error)
-}
 
-}
-
-
+  function reset() {
+    $('#beforeClick').toggle()
+    $('#afterClick').toggle()
+    $('#afterClicks').toggle()
+    $('.onReset').toggle()
+    $('#query').val("")
+  }
 
 
 })
